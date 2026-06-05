26e5c4f5e3b1471096fc1fceead606da50c59995
floki0425 <evangelistajoshua38@gmail.com>
2026-06-05 20:51:17 +0800

    Add scroll restoration for route navigation

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+function ScrollToTop() {
+  const { pathname, hash } = useLocation()
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.querySelector(hash)
+
+      if (element) {
+        window.requestAnimationFrame(() => {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+        })
+      }
+
+      return
+    }
+
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'instant',
+    })
+  }, [pathname, hash])
+
+  return null
+}
+
+export default ScrollToTop
