995e4fce7b52cfbc444f68a7377ef55c7bcedc35
floki0425 <evangelistajoshua38@gmail.com>
2026-06-05 20:52:27 +0800

    Update Supabase client and utility helpers

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,3 +7,39 @@
 
 export const cx = (...classes) => classes.filter(Boolean).join(' ')
 
+export const tripDetailFields = ['pickup_location', 'pickup_date', 'return_date']
+
+export const cleanTripDetails = (details = {}) =>
+  tripDetailFields.reduce((cleaned, field) => {
+    const value = String(details[field] || '').trim()
+    if (value) cleaned[field] = value
+    return cleaned
+  }, {})
+
+export const getTripDetailsSearch = (details = {}) => {
+  const params = new URLSearchParams()
+  const cleanDetails = cleanTripDetails(details)
+
+  tripDetailFields.forEach((field) => {
+    if (cleanDetails[field]) params.set(field, cleanDetails[field])
+  })
+
+  return params.toString()
+}
+
+export const getTripDetailsFromSearch = (search = '') => {
+  const params = new URLSearchParams(search)
+
+  return cleanTripDetails({
+    pickup_location: params.get('pickup_location'),
+    pickup_date: params.get('pickup_date'),
+    return_date: params.get('return_date'),
+  })
+}
+
+export const getTripDetailsFromNavigation = (location) =>
+  cleanTripDetails({
+    ...(location?.state?.tripDetails || {}),
+    ...getTripDetailsFromSearch(location?.search),
+  })
+
